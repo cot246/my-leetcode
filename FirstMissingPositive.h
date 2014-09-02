8a68349e19391640787c787581f4f045d29f50fe
Shin <cot246@gmail.com>
2014-09-02 12:41:53 +0800

    Create FirstMissingPositive.h

---
+++ FirstMissingPositive.h
@@ -0,0 +1,19 @@
+/**
+ *time:O(n) space:O(1)
+ *桶排序
+ */
+class Solution {
+public:
+    int firstMissingPositive(int A[], int n) {
+        if(n==0)return 1;
+        for(int i=0;i<n;++i)
+        {
+            while(A[i]>0&&A[i]<=n&&A[i]!=i+1&&A[i]!=A[A[i]-1])swap(A[i],A[A[i]-1]);
+        }
+        for(int i=0;i<n;++i)
+        {
+            if(A[i]!=i+1)return i+1;
+        }
+        return n+1;
+    }
+};
